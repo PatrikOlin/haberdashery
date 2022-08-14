1bc142796c8aaf9b5bea0d24a3929819510ab081
Patrik Olin <enmejl@gmail.com>
2022-08-14 12:06:25 +0200

    change url

--- client/src/store.js
+++ client/src/store.js
@@ -3,18 +3,12 @@ import { writable } from 'svelte/store';
 export const garments = writable([]);
 export const isFetching = writable(false);
 
+const url = 'http://haberdashery.home.lan:4040/v1'
+
 export const getAllGarments = () => {
   isFetching.set(true);
-  return fetch('http://localhost:4040/v1/garments?includeOrphans=true').then((x) => {
+  return fetch(`${url}/garments?includeOrphans=true`).then((x) => {
     isFetching.set(false);
     return x.json();
   });
-}
-
-export const getAllOrphans = () => {
-  isFetching.set(true);
-  return fetch("http://localhost:4040/v1/garments?orphans=true").then((x) => {
-    isFetching.set(false);
-    return x.json();
-  })
 }
